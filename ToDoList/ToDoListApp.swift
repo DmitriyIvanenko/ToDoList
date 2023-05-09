951dae4412a460707eec5f1ab1865ccebe4b4a68
Dmytro Ivanenko <ivanenko2010@gmail.com>
2023-05-09 15:07:43 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Dmytro Ivanenko on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
