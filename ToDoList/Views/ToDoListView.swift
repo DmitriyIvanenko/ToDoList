5970bccf9b98a546447cc21c187b42fe85b396ba
Dmytro Ivanenko <ivanenko2010@gmail.com>
2023-05-29 14:55:49 +0300

    10 lesson

--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -5,21 +5,35 @@
 //  Created by Dmytro Ivanenko on 09.05.2023.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
     @StateObject var viewModel = ToDoListViewViewModel()
-    
-    private let userId: String
-    
+    @FirestoreQuery var items: [ToDoListItem]
+
     init(userId: String) {
-        self.userId = userId
+        self._items = FirestoreQuery(
+            collectionPath: "users/\(userId)/todos"
+        )
     }
     
     var body: some View {
         NavigationView {
             VStack {
-               
+                List(items) { item in
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button {
+                                viewModel.delete(id: item.id)
+                            } label: {
+                                Text("Delete")
+                                    .backgroundStyle(Color.red)
+                            }
+                            
+                        }
+                }
+                .listStyle(PlainListStyle())
             }
             .navigationTitle("To Do List")
             .toolbar {
@@ -39,6 +53,6 @@ struct ToDoListView: View {
 
 struct ToDoListItemsView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListView(userId: "")
+        ToDoListView(userId: "p2WwkyjWFZUwPRmxfjmppPVCB7e2")
     }
 }
