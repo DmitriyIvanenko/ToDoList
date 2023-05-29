e33b65693eac3ed5bc145243f1ab0e33f4787d58
Dmytro Ivanenko <ivanenko2010@gmail.com>
2023-05-29 17:06:40 +0300

    12 Lesson

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -5,12 +5,27 @@
 //  Created by Dmytro Ivanenko on 09.05.2023.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ProfileViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
+    
 }
