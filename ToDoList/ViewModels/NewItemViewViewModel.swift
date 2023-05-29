f2aec7d7aca24e8462c934a5ccfeaceda0e7cfe1
Dmytro Ivanenko <ivanenko2010@gmail.com>
2023-05-29 14:22:52 +0300

    Lesson 9

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Dmytro Ivanenko on 09.05.2023.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -15,7 +16,30 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else {
+            return
+        }
         
+        // Get current User ID
+        guard let uID = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // Create Model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId,
+                                   title: title,
+                                   dueDate: dueDtae.timeIntervalSince1970,
+                                   createDate: Date().timeIntervalSince1970,
+                                   isDone: false)
+        
+        // Save Model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uID)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
